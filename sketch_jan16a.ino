6f415a6178a7492b9c94d36e3016884c7f39c277
Rodoro <rodoro.game@gmail.com>
2025-02-06 11:50:00 +0300

    Add ik sensors

--- sketch_jan16a.ino
+++ sketch_jan16a.ino
@@ -30,6 +30,10 @@ const int stepsPerRevolution = 2038*3.2;
 
 Stepper myStepper(stepsPerRevolution, 10, 12, 11, 13);
 
+const int infraredSensor1  = 15; // Датчик на калитке
+const int infraredSensor2  = 16; // Датчик на воротах
+const int infraredSensor3  = 17; // Датчик на воротах
+
 void stopen() {
   digitalWrite(10, LOW);
   digitalWrite(11, LOW);
@@ -46,10 +50,14 @@ void setup() {
   myServo.write(servoCloseAngle); 
   myStepper.setSpeed(9);
   pinMode(18, OUTPUT);
+
+  pinMode(infraredSensor1, INPUT);
+  pinMode(infraredSensor2, INPUT);
+  pinMode(infraredSensor3, INPUT);
 }
 
 void loop() {
-  char key = keypad.getKey();
+    char key = keypad.getKey();
 
   if (Serial.available()) {
     String com = Serial.readStringUntil('\n');
@@ -63,10 +71,9 @@ void loop() {
     if (key == '*') { 
       Serial.println(inputCode);
       if (inputCode == correctPin) {
-        
         activateServo();
       } else {
-         digitalWrite(18, 1);
+        digitalWrite(18, 1);
         delay(1000);
         digitalWrite(18, 0);
         delay(100);
@@ -80,41 +87,27 @@ void loop() {
   }
 }
 
-void activateMoto() {
-  count=count+1;
-  Serial.println(count);
-  Serial.println(count%2);
-  if(count%2 == 0) {
-    digitalWrite(19, HIGH);
-    myStepper.step(stepsPerRevolution);// Открытие
-    stopen() ;
-    digitalWrite(19, LOW);
-    delay(2000);
-    digitalWrite(19, HIGH);
-    myStepper.step(-stepsPerRevolution/2);// Закрытие
-    stopen() ;
-    digitalWrite(19, LOW);
-    delay(1000);
-    digitalWrite(19, HIGH);
-    myStepper.step(stepsPerRevolution/2);
-    stopen() ;
-    digitalWrite(19, LOW);
-    delay(2000);
-    digitalWrite(19, HIGH);
-    myStepper.step(-stepsPerRevolution);
-    stopen() ;
-    digitalWrite(19, LOW);
+int readInfraredSensor(int sensorPin) {
+  int value = analogRead(sensorPin);
+  return value;
+}
+
+bool isGateOpen() {
+  int value = readInfraredSensor(infraredSensor1);
+  if (value > 500) { 
+    return true;
   } else {
-    digitalWrite(19, HIGH);
-    myStepper.step(stepsPerRevolution);// Открытие
-    stopen() ;
-    digitalWrite(19, LOW);
-    delay(2000);
-    digitalWrite(19, HIGH);
-    myStepper.step(-stepsPerRevolution);// Закрытие
-    stopen() ;
-    digitalWrite(19, LOW);
-    delay(1000);
+    return false;
+  }
+}
+
+bool areGatesOpen() {
+  int value1 = readInfraredSensor(infraredSensor2);
+  int value2 = readInfraredSensor(infraredSensor3);
+  if (value1 > 500 && value2 > 500) {
+    return true;
+  } else {
+    return false;
   }
 }
 
@@ -123,76 +116,31 @@ void activateServo() {
   delay(100);
   digitalWrite(18, 0);
   delay(100);
-  digitalWrite(18, 1);
-  delay(100);
-  digitalWrite(18, 1);
-  delay(100);
-  digitalWrite(18, 0);
-  delay(100);
-  digitalWrite(18, 1);
-  delay(100);
-  myServo.write(servoOpenAngle);
-  digitalWrite(18, 1);
-  delay(100);
-  digitalWrite(18, 0);
-  delay(100);
-  digitalWrite(18, 1);
-  delay(100);
-  digitalWrite(18, 0);
-  delay(100);
-  digitalWrite(18, 1);
-  delay(100);
-  digitalWrite(18, 0);
-  delay(100);
-  digitalWrite(18, 1);
-  delay(100);
-  digitalWrite(18, 0);
-  delay(100);
-  digitalWrite(18, 1);
-  delay(100);
-  digitalWrite(18, 0);
-  delay(100);
+  while (!isGateOpen()) {
+    myServo.write(servoOpenAngle);
+    delay(100);
+  }
   delay(openTime);
-  digitalWrite(18, 1);
-  delay(100);
-  digitalWrite(18, 0);
-  delay(100);
-  digitalWrite(18, 1);
-  delay(100);
-  digitalWrite(18, 0);
-  delay(100);
-  digitalWrite(18, 1);
-  delay(100);
-  digitalWrite(18, 0);
-  delay(100);
-  digitalWrite(18, 1);
-  delay(100);
-  digitalWrite(18, 0);
-  delay(100);
-  digitalWrite(18, 1);
-  delay(100);
-  digitalWrite(18, 0);
-  delay(100);
-  digitalWrite(18, 1);
-  delay(100);
-  digitalWrite(18, 0);
-  delay(100);
-  digitalWrite(18, 1);
-  delay(100);
-  digitalWrite(18, 0);
-  delay(100);
-  digitalWrite(18, 1);
-  delay(100);
-  digitalWrite(18, 0);
-  delay(100);
-  digitalWrite(18, 1);
-  delay(100);
-  digitalWrite(18, 0);
-  delay(100);
-  digitalWrite(18, 1);
-  delay(100);
-  digitalWrite(18, 0);
-  delay(100);
   myServo.write(servoCloseAngle);
-  delay(openTime);// Закрытие
+  delay(openTime);
+}
+
+void activateMoto() {
+  digitalWrite(19, HIGH);
+  myStepper.step(stepsPerRevolution);
+  stopen();
+  digitalWrite(19, LOW);
+  delay(2000);
+  digitalWrite(19, HIGH);
+  myStepper.step(-stepsPerRevolution);
+  stopen();
+  digitalWrite(19, LOW);
+  delay(1000);
+  while (!areGatesOpen()) {
+    myServo.write(servoOpenAngle);
+    delay(100);
+  }
+  delay(openTime);
+  myServo.write(servoCloseAngle);
+  delay(openTime);
 }
